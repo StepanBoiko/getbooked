5a3f43aa3f0d7736cc7e52ca62e07887b960c24b
Stepan Boiko <stepko.b@gmail.com>
2024-01-16 20:55:04 -0500

    deploy update

--- app/scripts/deploy_staging.sh
+++ app/scripts/deploy_staging.sh
@@ -2,25 +2,27 @@
 
 echo "Deploying to Staging..."
 
-APP_DIR="/path/to/your/app"
-RAILS_ENV="staging"
+APP_NAME="getbooked"
+REMOTE_USER="ec2-user"
+REMOTE_HOST="44.199.170.53"
+REPO_URL="https://github.com/StepanBoiko/getbooked.git"
+BRANCH="master"
 
-cd $APP_DIR
+# Clone or pull the latest code from the repository
+ssh ${REMOTE_USER}@${REMOTE_HOST} "mkdir remotetestdir"
+ssh ${REMOTE_USER}@${REMOTE_HOST} "cd temotetestdir"
+ssh ${REMOTE_USER}@${REMOTE_HOST} "git clone https://github.com/StepanBoiko/getbooked.git"
+# ssh ${REMOTE_USER}@${REMOTE_HOST} "git clone ${REPO_URL} -b ${BRANCH} --single-branch --depth 1 ${APP_NAME} || (cd ${APP_NAME} && git pull)"
 
-# Fetch the latest changes from the repository
-git pull origin main
+# # Change to the application directory
+# ssh ${REMOTE_USER}@${REMOTE_HOST} "cd ${APP_NAME}"
 
-# Install or update dependencies
-bundle install --deployment --without development test
+# # Install dependencies and run migrations
+# ssh ${REMOTE_USER}@${REMOTE_HOST} "cd ${APP_NAME} && bundle install --without development test && rake db:migrate"
 
-# Run database migrations
-RAILS_ENV=$RAILS_ENV bundle exec rake db:migrate
+# # Restart the application server (replace with your actual server restart command)
+# ssh ${REMOTE_USER}@${REMOTE_HOST} "sudo systemctl restart your_rails_app.service"
 
-# Precompile assets
-RAILS_ENV=$RAILS_ENV bundle exec rake assets:precompile
-
-# Restart the application server
-sudo systemctl restart your-app-staging.service
 
 echo "Deployment to staging completed successfully."
 
